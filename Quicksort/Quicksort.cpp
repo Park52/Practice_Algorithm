daa54259bb9bdf13c61045a17d0f7aa8a6e578d4
Park52 <wogus8660@gmail.com>
2019-11-13 09:45:25 +0900

    Quicksort

---
+++ Quicksort/Quicksort.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+#define MAX_SIZE	9
+#define SWAP(x,y,temp) ((temp) =(x), (x)= (y), (y)=(temp))
+
+using namespace std;
+
+int partition(int list[], int left, int right)
+{
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = list[left];
+
+	do {
+		do {
+			low++;
+		} while (low <= right && list[low] < pivot);
+
+		do {
+			high--;
+		} while (high >= left && list[high] > pivot);
+
+		if (low < high)
+		{
+			SWAP(list[low], list[high], temp);
+		}
+	} while (low < high);
+
+	SWAP(list[left], list[high], temp);
+
+	return high;
+}
+
+void quick_sort(int list[], int left, int right)
+{
+	if (left < right)
+	{
+		int q = partition(list, left, right);
+
+		quick_sort(list, left, q - 1);
+		quick_sort(list, q + 1, right);
+	}
+}
+int main(void)
+{
+	int nSize = MAX_SIZE;
+	int list[MAX_SIZE] = { 5,3,8,4,9,1,6,2,7 };
+
+	quick_sort(list, 0, nSize - 1);
+
+	for (int i = 0; i < nSize; i++)
+	{
+		printf("%d ", list[i]);
+	}
+	return 0;
+}
